e64ba3d4bec2677d137919c5b3373572d5acc9a7
Bridgit-wairimu <kirikabetty@gmail.com>
2021-02-02 04:32:56 +0300

    created the urls for the login,register and logout

--- todoapp/urls.py
+++ todoapp/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns=[
     url('^$',views.index,name = 'index'),
+    url(r'^register/', views.registerPage, name="register"),
+    url(r'^login/', views.loginPage, name="login"),
+    url(r'^logout/', views.logoutUser, name="logout"),
+    
 ]
