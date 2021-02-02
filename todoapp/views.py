5d69b63dcda3783de05ffa70aee7908878e0768a
Bridgit-wairimu <kirikabetty@gmail.com>
2021-02-02 04:33:39 +0300

    created the views for the login,register and logout

--- todoapp/views.py
+++ todoapp/views.py
@@ -1,5 +1,50 @@
-from django.http  import HttpResponse
+from django.shortcuts import render,redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm
+from django.views.decorators.csrf import csrf_exempt
+from .forms import CreateUserForm
+
 
 # Create your views here.
 def index(request):
-    return HttpResponse('Welcome to the Moringa Tribune')
+    return render(request, 'index.html')
+
+
+@csrf_exempt
+def registerPage(request):
+    if request.user.is_authenticated:
+        return redirect('index')
+    else:
+        form = CreateUserForm()
+        if request.method == 'POST':
+            form =CreateUserForm(request.POST)
+            if form.is_valid():
+                form.save()
+            
+                messages.success(request,'Account was created successfully')
+                return redirect('login')
+        context = {'form': form}
+        return render(request,'registration/register.html',  context)
+
+def loginPage(request):
+    if request.user.is_authenticated:
+        return redirect('index')
+    else:
+        if request.method == 'POST':
+            username=request.POST.get('username')
+            password=request.POST.get('password')
+            user = authenticate(request, username=username ,password=password)
+            
+            if user is not None:   
+                login(request, user)
+               
+        context={}
+        return render(request,'registration/login.html',  context)
+
+def logoutUser(request):
+    logout(request)
+    return redirect('login')    
+
