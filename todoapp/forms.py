63d90d43f6bb1b51199692fa75348b6ed30b16a1
Bridgit-wairimu <kirikabetty@gmail.com>
2021-02-03 16:33:20 +0300

    created a TasksForm

--- todoapp/forms.py
+++ todoapp/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.forms import ModelForm
 from django.contrib.auth.models import User
-from . models import *
+from . models import Task,Profile
 
 class CreateUserForm(UserCreationForm):
     class Meta:
