3df2815778a747578c4792a508e9bd194d7bccc4
Bridgit-wairimu <kirikabetty@gmail.com>
2021-02-03 11:14:45 +0300

    created the url for the profile and adding a new task

--- todoapp/urls.py
+++ todoapp/urls.py
@@ -1,10 +1,20 @@
 from django.conf.urls import url
 from . import views
 
+from django.conf import settings
+from django.conf.urls.static import static
+from .views import TaskCreateView
+
+
 urlpatterns=[
     url('^$',views.index,name = 'index'),
+    url(r'^profile/', views.profile, name='profile'),
     url(r'^register/', views.registerPage, name="register"),
     url(r'^login/', views.loginPage, name="login"),
     url(r'^logout/', views.logoutUser, name="logout"),
-    
+    url(r'^task/new/', TaskCreateView.as_view(), name='task-create'),    
+
 ]
+
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
