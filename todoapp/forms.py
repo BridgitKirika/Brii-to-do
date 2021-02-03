ec9b79d5b320d2d1f640a09684dc74ad1f6ba6f3
Bridgit-wairimu <kirikabetty@gmail.com>
2021-02-03 11:13:37 +0300

    created a class for the UserUpdateForm ,ProfileUpdateForm and the TasksForm

--- todoapp/forms.py
+++ todoapp/forms.py
@@ -2,9 +2,28 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.forms import ModelForm
 from django.contrib.auth.models import User
-
+from . models import *
 
 class CreateUserForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username','email','password1','password2']
+
+class UserUpdateForm(forms.ModelForm):
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ['username', 'email']
+
+
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['image']
+
+
+class TasksForm(forms.ModelForm):
+    class Meta:
+        model = Task
+        fields = '__all__'
