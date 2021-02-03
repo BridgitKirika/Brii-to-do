e403a859bd96649456d2fb2b385289e8a64edada
Bridgit-wairimu <kirikabetty@gmail.com>
2021-02-03 10:46:20 +0300

    created views for the index and the profile

--- todoapp/views.py
+++ todoapp/views.py
@@ -1,16 +1,39 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
-from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
+from .forms import *
+from .models import Task,Profile
+from django.views.generic import CreateView, ListView
+from django.contrib.auth import authenticate,login,logout
 from django.views.decorators.csrf import csrf_exempt
-from .forms import CreateUserForm
 
 
-# Create your views here.
+
+# Create your views here
+@login_required(login_url='login')
 def index(request):
-    return render(request, 'index.html')
+    tasks = Task.objects.all()
+
+    form = TasksForm()
+
+    if request.method == 'POST':
+        form = TasksForm(request.POST)
+        if form.is_valid():
+            form.save()
+        return redirect('/')
+
+    context = {'tasks':tasks, 'form': form}
+    return render(request, 'index.html', context)
+	
+
+
+
+class TaskCreateView(CreateView):
+    model = Task
+    template_name = 'update_task.html'   
+    fields= ['title','complete']    
 
 
 @csrf_exempt
@@ -23,12 +46,12 @@ def registerPage(request):
             form =CreateUserForm(request.POST)
             if form.is_valid():
                 form.save()
-            
                 messages.success(request,'Account was created successfully')
                 return redirect('login')
         context = {'form': form}
-        return render(request,'registration/register.html',  context)
+    return render(request,'registration/register.html',  context)
 
+@csrf_exempt
 def loginPage(request):
     if request.user.is_authenticated:
         return redirect('index')
@@ -37,14 +60,38 @@ def loginPage(request):
             username=request.POST.get('username')
             password=request.POST.get('password')
             user = authenticate(request, username=username ,password=password)
-            
             if user is not None:   
                 login(request, user)
-               
         context={}
-        return render(request,'registration/login.html',  context)
+        return render(request,'registration/login.html',  context) 
 
 def logoutUser(request):
     logout(request)
-    return redirect('login')    
+    return redirect('login')
+
+@login_required(login_url='login')
+def profile(request):
+    if request.method == 'POST':
+        u_form = UserUpdateForm(request.POST, instance=request.user)
+        p_form = ProfileUpdateForm(request.POST,
+                                   request.FILES,
+                                   instance=request.user.profile)
+        if u_form.is_valid() and p_form.is_valid():
+            u_form.save()
+            p_form.save()
+            messages.success(request, f'Your account has been updated!')
+            return redirect('profile')
+
+    else:
+        u_form = UserUpdateForm(instance=request.user)
+        p_form = ProfileUpdateForm(instance=request.user.profile)
+
+    context = {
+        'u_form': u_form,
+        'p_form': p_form
+    }
+
+    return render(request, 'profile.html', context)
+
+
 
